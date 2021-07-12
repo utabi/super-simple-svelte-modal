2274f206d1cd07d7690e208f1e7dde754e8102c3
utabi <utabie@gmail.com>
2021-07-12 19:00:34 +0900

    Add files via upload

---
+++ App.svelte
@@ -0,0 +1,20 @@
+<script>
+	
+	import Modal from './Modal.svelte';
+	let modal
+	
+	function openModal1(){
+		modal.command('open','type1')
+	}
+	function openModal2(){
+		modal.command('open','type2')
+	}
+	
+</script>
+
+<button on:click={openModal1}>OPEN MODAL 1</button>
+<button on:click={openModal2}>OPEN MODAL 2</button>
+<Modal bind:this={modal} />
+<style>
+	
+</style>
